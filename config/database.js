874b21120ee889e1d12457114533b155239e6ee5
TOURE SOULEYMANE <toure5013@gmail.com>
2020-12-07 16:54:07 +0100

    update the env file port 27017, create the database and add the name

--- config/database.js
+++ config/database.js
@@ -26,9 +26,9 @@ module.exports = {
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', 27017),
-      username: Env.get('DB_USER', 'admin'),
+      username: Env.get('DB_USER', ''),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis'),
+      database: Env.get('DB_DATABASE', 'expressdisposed'),
       options: {
         // replicaSet: Env.get('DB_REPLICA_SET', '')
         // ssl: Env.get('DB_SSL, '')
